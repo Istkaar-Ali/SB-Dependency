97013ffcae425a31dc6bf76812cafabd966cad05
Istkaar <istkaar.ali2022@gmail.com>
2023-12-16 12:32:41 +0530

    this is my first project

---
+++ src/main/java/com/springboot/Sb1DependencyApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1DependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1DependencyApplication.class, args);
+		System.out.println("Hello World !!");
+	}
+
+}
